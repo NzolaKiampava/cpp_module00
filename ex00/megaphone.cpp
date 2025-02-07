3d7c36b12bec18ee02d699e7eaeb469463e82f68
Nzola Kiampava <nzolakiampava@gmail.com>
2025-02-07 07:51:23 +0000

    Add files via upload

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 16:34:38 by nkiampav          #+#    #+#             */
+/*   Updated: 2025/01/29 16:34:42 by nkiampav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNREARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)toupper(argv[i][j]);
+        if (i != argc - 1)
+            std::cout << " ";
+    }
+    std::cout << std::endl;
+    return (0);
+}
