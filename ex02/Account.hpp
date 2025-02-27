b2fb4107654bfd1384d0fcf2433d5a7357054f56
Nzola Kiampava <nzolakiampava@gmail.com>
2025-02-27 08:51:16 +0100

    Update Account.hpp

--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -10,4 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 18:16:39 by nkiampav          #+#    #+#             */
+/*   Updated: 2025/02/21 18:17:07 by nkiampav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ACCOUNT_HPP
+#define ACCOUNT_HPP
+
+class Account {
+public:
+    typedef Account t;
+
+    static void displayAccountsInfos(void);
+    static int getNbAccounts(void);
+    static int getTotalAmount(void);
+    static int getNbDeposits(void);
+    static int getNbWithdrawals(void);
+
+    Account(int initial_deposit);
+    ~Account(void);
+
+    void makeDeposit(int deposit);
+    bool makeWithdrawal(int withdrawal);
+    int checkAmount(void) const;
+    void displayStatus(void) const;
+
+private:
+    static int _nbAccounts;
+    static int _totalAmount;
+    static int _totalNbDeposits;
+    static int _totalNbWithdrawals;
+
+    static void _displayTimestamp(void);
+
+    int _accountIndex;
+    int _amount;
+    int _nbDeposits;
+    int _nbWithdrawals;
+
+    Account(void);
+};
+
+#endif
